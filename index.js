230d938f365d600604f926a46b3ee533e2c1393b
hank <hank@henrygd.me>
2017-02-06 14:41:09 -0500

    simplify toggling of loader & caption

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
     // popup video element
     displayVideo,
     // container element to hold youtube / vimeo iframe
-    displaySiteVid,
+    iframeContainer,
     // iframe to hold youtube / vimeo player
     iframeSiteVid,
     // store requested image source
@@ -31,10 +31,10 @@
     loadingIcon,
     // caption element
     caption,
-    // store caption display state
-    captionDisplayed,
-    // caption text content
+    // caption content element
     captionText,
+    // store caption content
+    captionContent,
     // hide caption button element
     captionHideButton,
     // used during close animation to avoid triggering timeout twice
@@ -45,8 +45,6 @@
     cached,
     // store whether image requested is remote or local
     remoteImage,
-    // holds caption shown state
-    hasCaption,
     // Save bytes in the minified version
     doc = document,
     appendEl = 'appendChild',
@@ -59,55 +57,52 @@
     timeout = global.setTimeout;
 
 
-  module.exports = function(opts) {
+  module.exports = function(options) {
     // store video id if youtube / vimeo video is requested
-    var siteVid = opts.ytSrc || opts.vimeoSrc;
+    siteVidID = options.ytSrc || options.vimeoSrc;
 
     // called on initial open to create elements / style / add event handlers
-    !initialized && initialize();
+    initialized || initialize();
 
     // clear currently loading stuff
     if (isLoading) {
       global.clearTimeout(checkVidTimeout);
-      hideLoadingIcon();
       removeContainer();
     }
 
     // set trigger element
-    el = opts.el;
+    el = options.el;
 
     // wipe existing remoteImage state
     remoteImage = false;
 
     // set caption if provided
-    hasCaption = el.hasAttribute('caption');
-    if (hasCaption) {
-      captionText[htmlInner] = el.attributes.caption.value;
+    captionContent = el.getAttribute('caption');
+    if (captionContent) {
+      captionText[htmlInner] = captionContent;
       container[appendEl](caption);
-      captionDisplayed = true;
     }
 
     // if vimeo or youtube video
-    if (siteVid) {
-      showLoadingIcon();
-      siteVidID = siteVid;
-      displayElement = displaySiteVid;
-      createIframe(!!opts.ytSrc);
+    if (siteVidID) {
+      toggleLoadingIcon(true);
+      displayElement = iframeContainer;
+      createIframe(!!options.ytSrc);
     }
     // if remote image
-    else if (opts.imgSrc) {
+    else if (options.imgSrc) {
       remoteImage = true;
-      imgSrc = opts.imgSrc;
+      imgSrc = options.imgSrc;
       cached = ~imgCache.indexOf(imgSrc);
-      !cached && showLoadingIcon();
+      !cached && toggleLoadingIcon(true);
       displayElement = displayImage;
       displayElement.src = imgSrc;
     }
     // if direct video link
-    else if (opts.vidSrc) {
-      showLoadingIcon();
+    else if (options.vidSrc) {
+      toggleLoadingIcon(true);
       displayElement = displayVideo;
-      displayElement.src = opts.vidSrc;
+      displayElement.src = options.vidSrc;
       checkVid();
     }
     // local image / background image already loaded on page
@@ -139,8 +134,9 @@
     imgCache = [];
 
     // add style
+    // if you want to tweak, grab from doc head & run through beautifier
     var style = doc[createEl]('STYLE');
-    style[htmlInner] = '#bp_caption,#bp_container{bottom:0;left:0;right:0;position:fixed;opacity:0}#bp_container>*,.bp-x,#bp_loader{position:absolute;right:0}#bp_container{top:0;z-index:9999;background:rgba(0,0,0,.7);opacity:0;pointer-events:none;transition:opacity .35s}#bp_loader{top:0;left:0;bottom:0;display:-webkit-flex;display:flex;margin:0;cursor:wait;z-index:9}#bp_loader svg{width:40%;max-height:40%;margin:auto;' + webkitify('animation:', 'ldr .7s infinite linear;') + '}' + webkitifyKeyframes('keyframes ldr{to{' + webkitify('transform:', 'rotate(360deg);') + '}}') + '#bp_img,#bp_sv,#bp_vid{max-height:96%;max-width:96%;top:0;bottom:0;left:0;margin:auto;box-shadow:0 0 3em rgba(0,0,0,.4);z-index:-1}#bp_sv{width:171vh}#bp_caption{font-size:.9em;padding:1.3em;background:rgba(20,20,20,.97);color:#fff;text-align:center;transition:opacity .3s}.bp-x{font-family:Arial;top:0;cursor:pointer;opacity:.85;font-size:3em;padding:0 .3em;color:#fff}#bp_caption .bp-x{left:2%;top:auto;right:auto;bottom:100%;padding:0 .6em;background:#d74040;border-radius:2px 2px 0 0;font-size:2.3em}.bp-x:hover{opacity:1}@media (max-aspect-ratio:9/5){#bp_sv{height:53vw}}';
+    style[htmlInner] = '#bp_caption,#bp_container{bottom:0;left:0;right:0;position:fixed;opacity:0}#bp_container>*,.bp-x,#bp_loader{position:absolute;right:0}#bp_container{top:0;z-index:9999;background:rgba(0,0,0,.7);opacity:0;pointer-events:none;transition:opacity .35s}#bp_loader{top:0;left:0;bottom:0;display:-webkit-flex;display:flex;margin:0;cursor:wait;z-index:9}#bp_loader svg{width:40%;max-height:40%;margin:auto;' + webkitify('animation:', 'ldr .7s infinite linear;') + '}' + webkitifyKeyframes('keyframes ldr{to{' + webkitify('transform:', 'rotate(1turn);') + '}}') + '#bp_container img,#bp_sv,#bp_vid{max-height:96%;max-width:96%;top:0;bottom:0;left:0;margin:auto;box-shadow:0 0 3em rgba(0,0,0,.4);z-index:-1}#bp_sv{width:171vh}#bp_caption{font-size:.9em;padding:1.3em;background:rgba(15,15,15,.94);color:#fff;text-align:center;transition:opacity .3s}.bp-x{font-family:Arial;top:0;cursor:pointer;opacity:.8;font-size:3em;padding:0 .3em;color:#fff}#bp_caption .bp-x{left:2%;top:auto;right:auto;bottom:100%;padding:0 .6em;background:#d74040;border-radius:2px 2px 0 0;font-size:2.3em}.bp-x:hover{opacity:1}@media (max-aspect-ratio:9/5){#bp_sv{height:53vw}}';
     doc.head[appendEl](style);
 
     // create container element
@@ -151,7 +147,6 @@
 
     // create display image element
     displayImage = doc[createEl]('IMG');
-    displayImage.id = 'bp_img';
 
     // create display video element
     displayVideo = doc[createEl]('VIDEO');
@@ -164,7 +159,13 @@
     caption = doc[createEl]('DIV');
     caption.id = 'bp_caption';
     captionHideButton = createCloseButton();
-    captionHideButton.onclick = hideCaption;
+    captionHideButton.onclick = function() {
+      changeCSS(caption, 'opacity:0');
+      timeout(function() {
+        captionContent = false;
+        container[removeEl](caption);
+      }, 300);
+    }
     caption[appendEl](captionHideButton);
     captionText = doc[createEl]('SPAN');
     caption[appendEl](captionText);
@@ -175,25 +176,23 @@
     loadingIcon[htmlInner] = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 399 399"><path d="M341 58.5C303 20.8 253 0 199.6 0 146.4 0 96.2 20.8 58.5 58.5 20.8 96.2 0 146.5 0 199.7 0 253 20.8 303.2 58.5 341c37.7 37.6 88 58.4 141.2 58.4 53.3 0 103.5-20.8 141.2-58.5 37.6-37.8 58.4-88 58.4-141.3 0-53.3-20.8-103.5-58.5-141.2zm-13 12.8c34.3 34.3 53.2 80 53.2 128.4h-41c0-77.4-63-140.4-140.5-140.4-4.6 0-9 .2-13.6.7V18.7c4.6-.4 9-.5 13.7-.5 48.5 0 94 18.8 128.4 53zM199.8 322c-67.4 0-122.2-55-122.2-122.3S132.3 77.5 199.7 77.5 322 132.3 322 199.7 267 322 199.6 322z"/></svg>';
 
     // create youtube / vimeo container
-    displaySiteVid = doc[createEl]('DIV');
-    displaySiteVid.id = 'bp_sv';
+    iframeContainer = doc[createEl]('DIV');
+    iframeContainer.id = 'bp_sv';
 
     // create iframe to hold youtube / vimeo player
     iframeSiteVid = doc[createEl]('IFRAME');
     iframeSiteVid.allowFullscreen = true;
     iframeSiteVid.onload = open;
     changeCSS(iframeSiteVid, 'border:0px;height:100%;width:100%');
-    displaySiteVid[appendEl](iframeSiteVid);
+    iframeContainer[appendEl](iframeSiteVid);
 
     // display image bindings for image load and error
     displayImage.onload = open;
-    displayImage.onerror = function() {
-      open('image');
-    };
+    displayImage.onerror = open.bind(null, 'image');
 
     // adjust loader position on window resize
     global.addEventListener('resize', function() {
-      isLoading && showLoadingIcon();
+      isLoading && toggleLoadingIcon(true);
     });
 
     // all done
@@ -232,31 +231,27 @@
     else if (displayVideo.error)
       open('video');
     else
-      checkVidTimeout = timeout(checkVid, 40);
+      checkVidTimeout = timeout(checkVid, 35);
   }
 
 
-  // show loading icon on top of trigger element
-  function showLoadingIcon() {
-    isLoading = true;
-    changeCSS(loadingIcon, 'top:' + el.offsetTop +
-     'px;left:' + el.offsetLeft + 'px;height:' +
-      el[cHeight] + 'px;width:' + el[cWidth] + 'px');
-    el.parentElement[appendEl](loadingIcon);
-  }
-
-
-  // hide loading icon
-  function hideLoadingIcon() {
-    isLoading = false;
-    el.parentElement[removeEl](loadingIcon);
+// hide / show loading icon
+  function toggleLoadingIcon(bool) {
+    // bool is true if we want to show icon, false if we want to remove
+    // change style to match trigger element dimensions if we want to show
+    bool && changeCSS(loadingIcon, 'top:' + el.offsetTop +
+       'px;left:' + el.offsetLeft + 'px;height:' +
+        el[cHeight] + 'px;width:' + el[cWidth] + 'px');
+    // add or remove loader from DOM
+    el.parentElement[bool ? appendEl : removeEl](loadingIcon);
+    isLoading = bool;
   }
 
 
   // animate open of image / video; display caption if needed
   function open(err) {
     // hide loading spinner
-    isLoading && hideLoadingIcon();
+    isLoading && toggleLoadingIcon();
 
     // check if we have an error string instead of normal event
     if (typeof(err) === 'string') {
@@ -265,9 +260,7 @@
     }
 
     // if remote image is loaded, add url to imgCache array
-    if (remoteImage && !cached) {
-      imgCache.push(imgSrc);
-    }
+    (remoteImage && !cached) && imgCache.push(imgSrc);
 
     // transform displayEl to match trigger el
     changeCSS(displayElement, getRect());
@@ -278,33 +271,16 @@
     // enlarge displayEl, fade in caption if hasCaption
     timeout(function() {
       changeCSS(displayElement, webkitify('transition:', 'transform .35s;') + webkitify('transform:', 'none;'));
-      if (hasCaption) {
-        timeout(function() {
-          changeCSS(caption, 'opacity:1');
-        }, 250);
-      }
+      captionContent && timeout(changeCSS.bind(null, caption, 'opacity:1'), 250);
     }, 60);
-  }
-
-
-  // hide caption
-  function hideCaption() {
-    changeCSS(caption, pointerEventsAuto);
-    timeout(function() {
-      container[removeEl](caption);
-      captionDisplayed = false;
-    }, 250);
   }
 
 
   // close active display element
   function close(e) {
     // don't close if caption or video was clicked
-    var target = e.target;
-    if (target === caption
-      || target === captionHideButton
-      || /SPAN|VIDEO/.test(target.tagName)
-      || isClosing) {
+    var clickEls = [caption, captionHideButton, displayVideo, captionText];
+    if (isClosing || ~clickEls.indexOf(e.target)) {
       return;
     }
 
@@ -324,17 +300,14 @@
     doc.body[removeEl](container);
     container[removeEl](displayElement);
     changeCSS(container, '');
-    changeCSS(displayElement, '');
 
     // clear src of displayElement (or iframe if display el is iframe container)
-    (displayElement === displaySiteVid ? iframeSiteVid : displayElement)
+    (displayElement === iframeContainer ? iframeSiteVid : displayElement)
       .removeAttribute('src');
 
-    // remove caption
-    if (captionDisplayed) {
-      container[removeEl](caption);
+    if (captionContent) {
       changeCSS(caption, '');
-      captionDisplayed = false;
+      container[removeEl](caption);
     }
     isClosing = false;
   }
