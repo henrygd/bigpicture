5a2a3d4cb883ceb4cdc3f5f72b09a1aebc69be40
hank <hank@henrygd.me>
2019-07-14 16:48:08 -0400

    tabs > spaces

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
-let gulp = require('gulp')
-let uglify = require('gulp-uglify')
-let pump = require('pump')
-let rename = require('gulp-rename')
-let replace = require('gulp-replace')
+let gulp = require('gulp');
+let uglify = require('gulp-uglify');
+let pump = require('pump');
+let rename = require('gulp-rename');
+let replace = require('gulp-replace');
 
-gulp.task('uglify', function () {
+gulp.task('uglify', function() {
 	pump([
 		gulp.src('example_page/js/BigPicture.js'),
 		uglify(),
 		rename('BigPicture.min.js'),
-		gulp.dest('./dist')
-	])
-})
+		gulp.dest('./dist'),
+	]);
+});
 
-gulp.task('copy', function () {
+gulp.task('copy', function() {
 	pump([
 		gulp.src('example_page/js/BigPicture.js'),
 		rename('BigPicture.js'),
-		gulp.dest('./dist')
-	])
-})
+		gulp.dest('./dist'),
+	]);
+});
 
-gulp.task('index', function () {
+gulp.task('index', function() {
 	pump([
 		gulp.src('example_page/js/BigPicture.js'),
 		replace('global.BigPicture', 'module.exports'),
 		rename('index.js'),
-		gulp.dest('./')
-	])
-})
+		gulp.dest('./'),
+	]);
+});
 
-gulp.task('build', ['copy', 'index', 'uglify'])
+gulp.task('build', ['copy', 'index', 'uglify']);
