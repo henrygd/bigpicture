b82004a9d0b536a7cadf702847d0b3243fc2e26d
hank <hank@henrygd.me>
2019-11-09 18:56:35 -0500

    convert to rollup / es6

---
+++ rollup.config.js
@@ -0,0 +1,69 @@
+import { terser } from 'rollup-plugin-terser'
+import buble from 'rollup-plugin-buble'
+import size from 'rollup-plugin-size'
+
+const production = !process.env.ROLLUP_WATCH
+
+export default production
+	? [
+			{
+				input: 'src/BigPicture.js',
+				output: {
+					strict: false,
+					sourcemap: false,
+					format: 'cjs',
+					file: 'index.js',
+				},
+				plugins: [buble()],
+			},
+			{
+				input: 'src/BigPicture.js',
+				output: {
+					strict: false,
+					sourcemap: false,
+					format: 'iife',
+					name: 'BigPicture',
+					file: 'dist/BigPicture.js',
+				},
+				plugins: [buble()],
+			},
+			{
+				input: 'src/BigPicture.js',
+				output: {
+					strict: false,
+					sourcemap: false,
+					format: 'iife',
+					name: 'BigPicture',
+					file: 'dist/BigPicture.min.js',
+				},
+				plugins: [
+					buble(),
+					terser({
+						compress: {
+							booleans_as_integers: true,
+							pure_getters: true,
+						},
+					}),
+					size(),
+				],
+			},
+	  ]
+	: {
+			input: 'src/BigPicture.js',
+			output: {
+				sourcemap: true,
+				format: 'iife',
+				name: 'BigPicture',
+				file: 'example_page/js/BigPicture.js',
+				strict: false,
+			},
+			plugins: [
+				buble(),
+				terser({
+					compress: {
+						booleans_as_integers: true,
+						pure_getters: true,
+					},
+				}),
+			],
+	  }
